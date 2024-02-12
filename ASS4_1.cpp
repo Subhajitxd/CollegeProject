c343492dd1c4681a70affe626b52fa0d5c87afbd
Subhajit Samajpati <70563469+Subhajitxd@users.noreply.github.com>
2024-02-12 14:30:29 +0530

    Add files via upload

--- ASS4_1.cpp
+++ ASS4_1.cpp
@@ -1,30 +1,52 @@
 #include <stdio.h>
 
 int main() {
-
+    // Declare variables to store input numbers
     int num1, num2, num3;
-	printf("Enter the first number: ");
+
+    // Input three numbers
+    printf("Enter the first number: ");
     scanf("%d", &num1);
+
     printf("Enter the second number: ");
     scanf("%d", &num2);
+
     printf("Enter the third number: ");
     scanf("%d", &num3);
+
     int max = num1;
     int min = num1;
+    int middle = num1;
+
+
     if (num2 > max) {
         max = num2;
     }
+
     else if (num2 < min) {
         min = num2;
     }
+    else {
+        middle = num2;
+    }
+
     if (num3 > max) {
+        middle = max;  
         max = num3;
     }
     else if (num3 < min) {
+        middle = min;  
         min = num3;
     }
+
+    else {
+        middle = num3;
+    }
+
     printf("Maximum value: %d\n", max);
     printf("Minimum value: %d\n", min);
+    printf("Middle value: %d\n", middle);
+
     return 0;
 }
 
